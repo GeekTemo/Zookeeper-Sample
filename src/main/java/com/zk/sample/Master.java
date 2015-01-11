960b3a1e322d66ccc73c0dcbc1f9e29c6625fb74
ArtsCrafts <siseulemen@gmail.com>
2015-01-11 13:38:54 +0800

    Zookeeper Sample

---
+++ src/main/java/com/zk/sample/Master.java
@@ -0,0 +1,14 @@
+package com.zk.sample;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+/**
+ * Created by gongxingfa on 15/1/11.
+ */
+public class Master implements Watcher{
+    @Override
+    public void process(WatchedEvent e) {
+
+    }
+}
